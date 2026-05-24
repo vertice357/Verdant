720d07891c0a17d2e6ca4436ec4381db719b7092
vertice357 <vertice357@gmail.com>
2026-05-23 20:33:03 -0400

    fix: resolve array literal parse error in GameScene wall definitions

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -41,25 +41,24 @@ export class GameScene extends Phaser.Scene {
     floor.fillRect(T * 2, T * 2, W - T * 4, H - T * 4)
 
     // Walls (visual)
-    const walls = this.add.graphics().setDepth(DEPTHS.FLOOR)
-    walls.fillStyle(0x1a2e28)
-    walls.fillRect(0, 0, W, T * 2)           // top
-    walls.fillRect(0, H - T * 2, W, T * 2)   // bottom
-    walls.fillRect(0, 0, T * 2, H)            // left
-    walls.fillRect(W - T * 2, 0, T * 2, H)   // right
-
-    // Stone border lines
-    walls.lineStyle(2, 0x374151, 1)
-    walls.strokeRect(T * 2, T * 2, W - T * 4, H - T * 4)
+    const wallGfx = this.add.graphics().setDepth(DEPTHS.FLOOR)
+    wallGfx.fillStyle(0x1a2e28)
+    wallGfx.fillRect(0, 0, W, T * 2)
+    wallGfx.fillRect(0, H - T * 2, W, T * 2)
+    wallGfx.fillRect(0, 0, T * 2, H)
+    wallGfx.fillRect(W - T * 2, 0, T * 2, H)
+    wallGfx.lineStyle(2, 0x374151, 1)
+    wallGfx.strokeRect(T * 2, T * 2, W - T * 4, H - T * 4)
 
     // Wall physics bodies
     this.walls = this.physics.add.staticGroup()
-    [
-      { x: W / 2,   y: T,       w: W,      h: T * 2 },
-      { x: W / 2,   y: H - T,   w: W,      h: T * 2 },
-      { x: T,       y: H / 2,   w: T * 2,  h: H     },
-      { x: W - T,   y: H / 2,   w: T * 2,  h: H     },
-    ].forEach(d => {
+    const wallDefs = [
+      { x: W / 2, y: T,       w: W,     h: T * 2 },
+      { x: W / 2, y: H - T,   w: W,     h: T * 2 },
+      { x: T,     y: H / 2,   w: T * 2, h: H     },
+      { x: W - T, y: H / 2,   w: T * 2, h: H     },
+    ]
+    wallDefs.forEach(d => {
       const zone = this.add.zone(d.x, d.y, d.w, d.h)
       this.physics.add.existing(zone, true)
       this.walls.add(zone)
@@ -73,7 +72,8 @@ export class GameScene extends Phaser.Scene {
     this.companion = new Companion(this, 420, 270, this.player)
 
     this.enemies = this.add.group()
-    ;[{ x: 700, y: 180 }, { x: 740, y: 380 }, { x: 200, y: 200 }].forEach(pos => {
+    const spawnPoints = [{ x: 700, y: 180 }, { x: 740, y: 380 }, { x: 200, y: 200 }]
+    spawnPoints.forEach(pos => {
       this.enemies.add(new Mossling(this, pos.x, pos.y, this.player))
     })
 
@@ -103,7 +103,11 @@ export class GameScene extends Phaser.Scene {
     // Aqua bolt
     this.physics.add.overlap(
       this.projectiles, this.enemies,
-      (proj, enemy) => { enemy.takeDamage(proj.damage); proj._gfx?.destroy(); proj.destroy() }
+      (proj, enemy) => {
+        enemy.takeDamage(proj.damage)
+        if (proj._gfx) proj._gfx.destroy()
+        proj.destroy()
+      }
     )
   }
 
